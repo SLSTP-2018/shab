ba2eeb67b45dc14f5b460f774b8a3804a577aeea
TheOneHyer <theonehyer@gmail.com>
2018-07-30 23:31:23 -0700

    Created Simple Read Loop in SHAB.ino
    Generated simple, non-functional loop to read data from altimeter
    module. No data is currently sent from the altimeter as it is currently
    miswired.

--- flight/SHAB/SHAB.ino
+++ flight/SHAB/SHAB.ino
@@ -1,9 +1,22 @@
 void setup() {
   // put your setup code here, to run once:
-
+  int SCL = 0;
+  int SDA = 1;
+  int VIN = 3;
+  
+  pinMode(SCL, OUTPUT);
+  pinMode(SDA, INPUT);
+  pinMode(VIN, OUTPUT);
+  Serial.begin(115200);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  int SCL = 0;
+  int SDA = 1;
+  int VIN = 3;
+  digitalWrite(VIN, HIGH);
+  if (Serial.available() > 0) {
+    Serial.println(Serial.read());
+  }
 }
