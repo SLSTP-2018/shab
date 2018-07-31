a9eaa019851531ae913e68c60af9e06da1f63a20
TheOneHyer <theonehyer@gmail.com>
2018-07-31 00:24:07 -0700

    Made Pin Variables Global
    Pin variables moved outside of functions. However, they conflict with
    internal names and need to have better names assigned.

--- flight/SHAB/SHAB.ino
+++ flight/SHAB/SHAB.ino
@@ -1,21 +1,19 @@
+//Globaly set pin names
+int SCL0 = 0;
+int SDA1 = 1;
+int VIN3 = 3;
+
 void setup() {
-  // put your setup code here, to run once:
-  int SCL = 0;
-  int SDA = 1;
-  int VIN = 3;
-  
-  pinMode(SCL, OUTPUT);
-  pinMode(SDA, INPUT);
-  pinMode(VIN, OUTPUT);
+  // put your setup code here, to run once:  
+  pinMode(SCL0, OUTPUT);
+  pinMode(SDA1, INPUT);
+  pinMode(VIN3, OUTPUT);
   Serial.begin(115200);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  int SCL = 0;
-  int SDA = 1;
-  int VIN = 3;
-  digitalWrite(VIN, HIGH);
+  digitalWrite(VIN3, HIGH);
   if (Serial.available() > 0) {
     Serial.println(Serial.read());
   }
