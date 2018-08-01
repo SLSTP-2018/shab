373c6ff4e5f11a834c720ed2a49709b0eca2cf1a
TheOneHyer <theonehyer@gmail.com>
2018-07-31 23:03:36 -0700

    Updated SHAB.ino for New Branch
    Created new branch for linear actuator code. Updated SHAB.ino for later
    coding in this branch.

--- flight/SHAB/SHAB.ino
+++ flight/SHAB/SHAB.ino
@@ -1,21 +1,13 @@
 void setup() {
-  // put your setup code here, to run once:
-  int SCL = 0;
-  int SDA = 1;
-  int VIN = 3;
-  
-  pinMode(SCL, OUTPUT);
+  // put your setup code here, to run once:  
   pinMode(SDA, INPUT);
-  pinMode(VIN, OUTPUT);
+  pinMode(SCL, OUTPUT);
+
   Serial.begin(115200);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  int SCL = 0;
-  int SDA = 1;
-  int VIN = 3;
-  digitalWrite(VIN, HIGH);
   if (Serial.available() > 0) {
     Serial.println(Serial.read());
   }
