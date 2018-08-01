79a07cac634fb3deff2541e6ba07b74515deecee
TheOneHyer <theonehyer@gmail.com>
2018-07-31 22:56:54 -0700

    Implemented Minimal Skeleton for Altimeter Module
    Very minimal code setup created. It simply sets up pins and prints any
    serial data received.

--- flight/SHAB/SHAB.ino
+++ flight/SHAB/SHAB.ino
@@ -1,19 +1,13 @@
-//Globaly set pin names
-int SCL0 = 0;
-int SDA1 = 1;
-int VIN3 = 3;
-
 void setup() {
   // put your setup code here, to run once:  
-  pinMode(SCL0, OUTPUT);
-  pinMode(SDA1, INPUT);
-  pinMode(VIN3, OUTPUT);
+  pinMode(SDA, INPUT);
+  pinMode(SCL, OUTPUT);
+  
   Serial.begin(115200);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(VIN3, HIGH);
   if (Serial.available() > 0) {
     Serial.println(Serial.read());
   }
