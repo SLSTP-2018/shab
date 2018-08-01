94a3e1053429dad316b905fbdac12924301cc26b
TheOneHyer <theonehyer@gmail.com>
2018-07-31 23:33:14 -0700

    Prevented Multiple Inclusion and Defined New Class
    ifndef and define preprocessor statements now prevent multiple inclusion
    of SHAB.ino. Added skeletal LinearActuator class to file.

--- flight/SHAB/SHAB.ino
+++ flight/SHAB/SHAB.ino
@@ -1,7 +1,17 @@
+#ifndef SHAB_SHAB_ino_  // Prevent multiple inclusion
+#define SHAB_SHAB_ino_  // Define if not defined previously
+
+class LinearActuator {
+  
+} tla, sla;
+
+// tla = Troposphere Linear Actuator
+// sla = Stratosphere Linear Actuator
+
 void setup() {
   // put your setup code here, to run once:  
-  pinMode(SDA, INPUT);
-  pinMode(SCL, OUTPUT);
+  pinMode(SDA, INPUT);  // This line may not be needed
+  pinMode(SCL, OUTPUT);  // This line may not be needed
 
   Serial.begin(115200);
 }
@@ -12,3 +22,5 @@ void loop() {
     Serial.println(Serial.read());
   }
 }
+
+#endif // SHAB_SHAB_ino_vv
