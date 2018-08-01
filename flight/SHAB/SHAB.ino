b3f11f14d2bb73736523fffcbef0981f36ebc2e2
TheOneHyer <theonehyer@gmail.com>
2018-07-31 23:42:47 -0700

    Included Copyright in SHAB.ino
    SHAB.ino now has a proper copyright notice.

--- flight/SHAB/SHAB.ino
+++ flight/SHAB/SHAB.ino
@@ -1,3 +1,23 @@
+/*
+  SHAB.ino runs two linear actuators based on altitude
+  Copyright (C) 2018  Alex Hyer, Andrew Jong
+ 
+  This file is part of SHAB.
+
+  SHAB is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  SHAB is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with SHAB.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #ifndef SHAB_SHAB_ino_  // Prevent multiple inclusion
 #define SHAB_SHAB_ino_  // Define if not defined previously
 
